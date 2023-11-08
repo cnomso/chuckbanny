25662c3e9439435f45e8362a538192c5dfb9de25
cnomso <cnomso@gmail.com>
2023-11-09 01:34:06 +0200

    CapStone first

--- README.md
+++ README.md
@@ -3,3 +3,43 @@
 
 This is a web portfolio of Chuck Banny a Digital Artist and Illustrator
 
+## Resources
+==================
+* Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+ * Copyright 2023 Fonticons, Inc.
+
+* Bootstrap v5.3.2 (https://getbootstrap.com/)
+ * Copyright 2011-2023 The Bootstrap Authors
+ * Copyright 2011-2023 Twitter, Inc.
+ * Licensed under MIT (https://github.com/twbs/bootstrap/blob/main/LICENSE)
+
+
+
+## Plan
+===========
+
+
+## Figma
+=========== 
+
+
+# WEBSITE
+=============
+
+
+## Header
+===========
+
+
+# About
+============
+
+# Skills
+
+
+# Portfolio
+===========
+
+# Contact
+============
---
+++ js/scripts.js
@@ -0,0 +1,85 @@
+
+function handleFormSubmit() {
+    // Prevent the default form submission behavior
+    document.getElementById("contactForm").addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        // Get the values of the name and email fields
+        const name = document.getElementById("cname").value;
+        const email = document.getElementById("cemail").value;
+        const message = document.getElementById("cmessage").value;
+
+        // Get the error element
+        const errorElement = document.getElementById("error");
+
+        // Check if name or email is empty or null
+        if (name === "" || name === null || email === "" || email === null || message == "" || message == null) {
+            // Display an error message
+            errorElement.textContent = "Please fill in all the fields";
+        } else {
+            // Clear any previous error message
+            errorElement.textContent = "";
+        }
+    });
+}
+
+(function($) {
+    "use strict"; 
+	
+    /* Navbar Scripts */
+    // jQuery to collapse the navbar on scroll
+    $(window).on('scroll load', function() {
+		if ($(".navbar").offset().top > 60) {
+			$(".fixed-top").addClass("top-nav-collapse");
+		} else {
+			$(".fixed-top").removeClass("top-nav-collapse");
+		}
+    });
+    
+	// jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+    });
+
+    // offcanvas script from Bootstrap + added element to close menu on click in small viewport
+    $('[data-toggle="offcanvas"], .navbar-nav li a:not(.dropdown-toggle').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
+
+ 
+    /* Move Form Fields Label When User Types */
+    // for input and textarea fields
+    $("input, textarea").keyup(function(){
+		if ($(this).val() != '') {
+			$(this).addClass('notEmpty');
+		} else {
+			$(this).removeClass('notEmpty');
+		}
+	});
+	
+
+    /* Back To Top Button */
+    // create the back to top button
+    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
+    var amountScrolled = 700;
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > amountScrolled) {
+            $('a.back-to-top').fadeIn('500');
+        } else {
+            $('a.back-to-top').fadeOut('500');
+        }
+    });
+
+
+	/* Removes Long Focus On Buttons */
+	$(".button, a, button").mouseup(function() {
+		$(this).blur();
+	});
+
+})(jQuery);
